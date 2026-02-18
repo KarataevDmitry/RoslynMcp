77921bf09d4e5a73c44831745629429848485974
Каратаев Дмитрий <dkarataev1990@gmail.com>
2026-02-18 20:54:41 +0300

    test(roslyn-mcp): add BreakpointResolveSample for resolve_breakpoint (constructor + indexer)

--- ServiceLayer/SyntheticRenameTest.cs
+++ ServiceLayer/SyntheticRenameTest.cs
@@ -15,3 +15,25 @@ public class SyntheticRenameTarget
     public static int StaticValue => 1;
     public int Value => 2;
 }
+
+/// <summary>Пример для теста roslyn_resolve_breakpoint: конструктор и индексатор.</summary>
+public class BreakpointResolveSample
+{
+    private int _value;
+
+    public BreakpointResolveSample()
+    {
+        _value = 0;
+    }
+
+    public BreakpointResolveSample(int initial)
+    {
+        _value = initial;
+    }
+
+    public int this[int index]
+    {
+        get => _value + index;
+        set => _value = value;
+    }
+}
